f0da0974e4d05a23c52ba40605aa22d453f0bb77
DangTrung <trungnd3605@gmail.com>
2026-04-09 23:27:41 +0700

    fix gradle : tu android 14 xuong 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.flight_booking_app"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.flight_booking_app"
-        minSdk = 30
-        targetSdk = 36
+        minSdk = 24
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -28,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
@@ -37,6 +35,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(libs.core.ktx)
+    implementation(libs.core)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
